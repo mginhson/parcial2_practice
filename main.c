481188659b1f51b4888e92e2faf66705efa6cdf6
mginhson <mginhson@itba.edu.ar>
2024-06-16 21:08:07 -0300

    structs test

--- main.c
+++ main.c
@@ -1,76 +1,69 @@
-/* TEST OF THREADS */
-
-//Compile  gcc -o th2 th2.c -lpthread
-
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdint.h>
 
+struct{
+    uint16_t a :15;
+    uint8_t  b  :1;
+}myvar;
 
-#define  ONE_MS    	1000	   // One mseg
-#define  time_delay(x)  (x/100)    // x in ms , minvalue 100mseg
-
-
-
-// ============ Colored Text ===========
-
-#define BLACK_TEXT		   "\e[30;1m"
-#define RED_TEXT		   "\e[31;1m"
-#define GREEN_TEXT 		   "\e[32;1m"
-#define YELLOW_TEXT 	   "\e[33;1m"
-#define BLUE_TEXT 		   "\e[34;1m"
-#define MAGENTA_TEXT 	   "\e[35;1m"
-#define CYAN_TEXT 	   	   "\e[36;1m"
-#define WHITE_TEXT 		   "\e[37m"
-
-// ============ Colored Text End ===========
-
-	
-//http://linuxprograms.wordpress.com/2007/12/29/threads-programming-in-linux-examples/
-
-
-enum{
-    WAITING,
-    TIMEOUT,
-
-}status_t = WAITING;
-
-unsigned int timerTick;
-
-void * th1(void *ptr)
+void rawPrintLiteral(size_t num, uint16_t size_in_bytes)
 {
-    while(1)
+    
+    uint8_t * ptr = (uint8_t*)&num; //endiannes independent
+    uint16_t i,j;
+    for (i = 0; i < size_in_bytes; i++,++ptr)
     {
-        usleep(ONE_MS * 100);
-        if(timerTick)
-            timerTick--;     
+        
+        printf("%p -> %#x ",ptr,*ptr);    
+        j = 7;
+
+        do{
+            (1<<j) & (*ptr) ? printf("1") : printf("0");
+        }while(j-- != 0);
+
+        printf("\n");
+
     }
+
+
+    printf("\n");
+
+    return;
 }
 
-void * th2(void * ptr)
+void rawPrint(const void *_ptr, uint16_t size_in_bytes)
 {
-    static int r=0;
-    while(1)
+    
+    const uint8_t * ptr = (uint8_t*)_ptr; //endiannes independent
+    uint16_t i,j;
+    for (i = 0; i < size_in_bytes; ++i,++ptr)
     {
-        if(!timerTick)
-        {
-            timerTick = time_delay(500);
-            printf("r = %d\n",r++);
-            
-        }
+        
+        printf("%p -> %#x ",ptr,(uint8_t)*ptr);    
+        j = 7;
+
+        do{
+            (1<<j) & (*ptr) ? printf("1") : printf("0");
+        }while(j-- != 0);
+
+        printf("\n");
+
     }
+
+
+    printf("\n");
+
+    return;
 }
+
 
 int main(int argc, char **argv)
 {
-    pthread_t thread1,thread2,thread3;
-    srand(time(NULL));
-    pthread_create(&thread1,NULL,th1,NULL);
-    pthread_create(&thread2,NULL,th2,NULL);
-    
-    pthread_join(thread1,NULL);
-    pthread_join(thread2,NULL);
-
-    return 0;
+    myvar.a = 0xABC;
+    myvar.b = 1;
+    rawPrintLiteral(0x12345678,sizeof(int));
+    rawPrint(&myvar,sizeof(myvar));
 }
